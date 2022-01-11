82bc8ed86ebeacc963d0f7ce4ab6345a01d0fcdc
Mert Güney <mert_guney_29@hotmail.com>
2022-01-11 20:36:56 +0300

    Strongly Typed hubs

---
+++ UdemySignalR.Api/Hubs/IProductHub.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using UdemySignalR.Api.Models;
+
+namespace UdemySignalR.Api.Hubs
+{
+    public interface IProductHub
+    {
+        Task ReceiveProduct(Product p);
+    }
+}
---
+++ UdemySignalR.Api/Hubs/ProductHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+using UdemySignalR.Api.Models;
+
+namespace UdemySignalR.Api.Hubs
+{
+    public class ProductHub : Hub<IProductHub>
+    {
+        public async Task SendProduct(Product p)
+        {
+            await Clients.All.ReceiveProduct(p);
+        }
+    }
+}
